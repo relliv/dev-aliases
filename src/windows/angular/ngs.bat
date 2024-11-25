97a2a74b701e90e5e4c512fe3a73690e9bd5c47f
Eyüp <epsilonelittr@gmail.com>
2024-11-25 03:41:06 +0300

    Remove angular commands

--- src/windows/angular/ngs.bat
+++
@@ -1,4 +0,0 @@
-@REM Serve Angular App
-
-@REM ng serve --host 0.0.0.0 --port 4200 --disable-host-check
-ng serve --host 0.0.0.0 --port 4200 --disable-host-check --live-reload=false
--- src/windows/angular/ngss.bat
+++
@@ -1,4 +0,0 @@
-@REM Serve Angular App in 4444 port
-
-@REM ng serve --port=4444 --host=0.0.0.0 --disable-host-check
-ng serve --port=4444 --host=0.0.0.0 --disable-host-check --live-reload=false
